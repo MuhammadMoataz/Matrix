d2804d044b4bf7f521a1029e7dcb45896a2940d2
Muhammad Moataz <mohamedmoatazali60@gmail.com>
2022-05-06 23:13:58 +0200

    added the algorithm

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include"matrix.h"
+using namespace std;
+int main()
+{
+    
+
+    return 0;
+}
---
+++ matrix.h
@@ -0,0 +1,7 @@
+#pragma once
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> to_vectr(int n, int m, int p);
+int matrix_vectr(int i, int j, int k, int m, int p);
