4983497d1f0683e14190ca86d36b91df47772302
Muhammad Moataz <mohamedmoatazali60@gmail.com>
2022-05-06 23:23:26 +0200

    Tested the algorithm

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include"matrix.h"
-using namespace std;
+
 int main()
 {
-    
+    // for instance, we will use a 3*3*3 matrix 
+    // for example: i = 2, j = 1 and k = 0, so the index is supposed to be 21
 
+    int n, m, p;
+    n = 3;
+    m = 3;
+    p = 3;
+
+    int i, j, k;
+    i = 2;
+    j = 1;
+    k = 0;
+    vector<int> vc = to_vectr(n, m, p);
+    int indx = matrix_vectr(i, j, k, m, p);
+    cout << indx << endl;
     return 0;
 }
